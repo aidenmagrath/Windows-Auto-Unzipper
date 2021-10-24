01c8828780549a21d11387cfd7309c94d698c405
aidenmagrath <aidenmagrath@gmail.com>
2021-10-24 15:40:33 +1030

    Created RegistryHelper class
    Will be used to enable/disable auto run

---
+++ Windows Auto Unzipper/RegistryHelper.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Windows_Auto_Unzipper
+{
+    class RegistryHelper
+    {
+
+        public static void EnableAutoRun() {
+            RegistryKey key = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+            key.SetValue(Application.ProductName, Application.ExecutablePath);
+        }
+
+        public static void DisableAutoRun()
+        {
+            RegistryKey key = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+            key.DeleteValue(Application.ProductName);
+        }
+    }
+}
